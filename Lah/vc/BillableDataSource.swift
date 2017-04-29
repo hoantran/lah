229b90b80b12043653129d4b3cf81dfd3bef1869
Hoan Tran <hoan@bluepego.com>
2017-04-29 09:45:14 -0700

    Connected  Billable Cell

---
+++ Lah/vc/BillableDataSource.swift
@@ -0,0 +1,58 @@
+//
+//  BillableDataSource.swift
+//  Lah
+//
+//  Created by Hoan Tran on 4/28/17.
+//  Copyright © 2017 Pego Consulting. All rights reserved.
+//
+import UIKit
+
+class BillableDataSource: NSObject {
+    let tableView: UITableView
+    
+    var billables: [Billable] = []
+
+    
+    init(tableView: UITableView, workerKey: String){
+        self.tableView = tableView
+        super.init()
+        self.tableView.dataSource = self
+        createFakeBillables(workerKey: workerKey)
+    }
+    
+    func createFakeBillables(workerKey: String) {
+        let time = Int(CFAbsoluteTimeGetCurrent())
+        let backtrack = 3 * 30 * 24 * 60 * 60 // about 3 months
+        let maxDuration = 24 * 60 * 60 // 1 day
+        for _ in 0...9 {
+            let start = Int.random(min: (time-backtrack), max: time)
+            let end = time + Int.random(min: 3600, max: maxDuration)
+            self.billables.append(Billable(rate: Float.random(min: 9.50, max: 15.87).roundTo(places: 2),
+                                           start: start,
+                                           end: end,
+                                           project: "-Kf_n1c1uH3Jf0abIdku",
+                                           worker: workerKey,
+                                           paid: end + 120,
+                                           note: "Hella job!" ))
+        }
+        
+        print(self.billables)
+    }
+
+    
+}
+
+
+extension BillableDataSource: UITableViewDataSource {
+
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.billables.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = Bundle.main.loadNibNamed("BillableCell", owner: self, options: nil)?.first as! BillableCellTableViewCell
+        cell.config(billable: self.billables[indexPath.row])
+        return cell
+    }
+
+}
--- Lah/vc/TimeCardViewController.swift
+++ Lah/vc/TimeCardViewController.swift
@@ -11,38 +11,29 @@ import UIKit
 class TimeCardViewController: UIViewController, EditedWorkerDelegate {
     var worker: Worker?
     static let TimeCardSegue = "EditWorker"
-    var billables: [Billable] = []
+    var dataSource: BillableDataSource?
+    var tableDelegate: BillableTableDelegate?
+    @IBOutlet weak var billableTable: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let workerKey = self.worker?.key {
+            self.dataSource = BillableDataSource(tableView: self.billableTable, workerKey: workerKey)
+            self.tableDelegate = BillableTableDelegate(tableView: self.billableTable, vc: self)
+        }
+        self.billableTable.reloadData()
+        print(self.dataSource ?? "no data source")
+        
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         guard let worker = self.worker, let lastName = self.worker?.lastName else { return }
         self.navigationItem.title = "\(worker.firstName) \(lastName)"
-        createFakeBillables()
-    }
-    
-    func createFakeBillables() {
-        guard let workerKey = self.worker?.key else { return }
-        let time = Int(CFAbsoluteTimeGetCurrent())
-        let backtrack = 3 * 30 * 24 * 60 * 60 // about 3 months
-        let maxDuration = 24 * 60 * 60 // 1 day
-        for _ in 0...9 {
-            let start = Int.random(min: (time-backtrack), max: time)
-            let end = time + Int.random(min: 3600, max: maxDuration)
-            self.billables.append(Billable(rate: Float.random(min: 9.50, max: 15.87).roundTo(places: 2),
-                                           start: start,
-                                           end: end,
-                                           project: "-Kf_n1c1uH3Jf0abIdku",
-                                           worker: workerKey,
-                                           paid: end + 120,
-                                           note: "Hella job!" ))
-        }
-        
-        print(self.billables)
-    }
+        self.billableTable.reloadData()
+        print(self.dataSource ?? "no data source 2")
+    }    
     
     @IBAction func cancel(_ sender: Any) {
         _ = self.navigationController?.popViewController(animated: true)
